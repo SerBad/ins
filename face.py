c862f186f6979f462dcf8450edc597f34fd62d4c
zhou <zhoutjcc@msn.cn>
2022-02-14 14:53:41 +0800

    save face tag

--- face.py
+++ face.py
@@ -14,8 +14,11 @@ import pandas
 def face(imagePath: str, faceRoot: str, bodyRoot: str):
     image = face_recognition.load_image_file(imagePath)
     face_locations = face_recognition.face_locations(image)
+    base_name = os.path.basename(imagePath)
     if len(face_locations) == 0:
         print(imagePath, "没有脸")
+        shutil.copyfile(imagePath, os.path.join(bodyRoot, base_name))
+        return 1, [base_name, []]
     else:
         for face_location in face_locations:
             # Print the location of each face in this image
@@ -23,9 +26,11 @@ def face(imagePath: str, faceRoot: str, bodyRoot: str):
             print(imagePath, bottom - top, right - left, "height:", image.shape[0], "width:", image.shape[1])
 
             if (bottom - top * 1.0) / image.shape[0] >= 0.4 and (right - left * 1.0) / image.shape[1] >= 0.4:
-                shutil.copyfile(imagePath, os.path.join(faceRoot, os.path.basename(imagePath)))
+                shutil.copyfile(imagePath, os.path.join(faceRoot, base_name))
+                return 2, [base_name, [top, right, bottom, left]]
             else:
-                shutil.copyfile(imagePath, os.path.join(bodyRoot, os.path.basename(imagePath)))
+                shutil.copyfile(imagePath, os.path.join(bodyRoot, base_name))
+                return 1, [base_name, [top, right, bottom, left]]
 
 
 class ImageOptions:
@@ -60,11 +65,32 @@ if __name__ == '__main__':
         os.makedirs(bodyRoot)
 
     executor = ProcessPoolExecutor(max_workers=8)
+    columns1 = ["image", "face"]
+    resultFaceData = []
+    resultBodyData = []
     futures = []
+
     for index1 in range(0, len(flist)):
         image = path + os.sep + flist[index1]
         # face(image, faceRoot, bodyRoot)
+
         task = executor.submit(face, image, faceRoot, bodyRoot)
         futures.append(task)
+
+    for future in as_completed(futures):
+        is_face, face = future.result()
+        if is_face == 1:
+            resultBodyData.append(face)
+        elif is_face == 2:
+            resultFaceData.append(face)
+        futures.remove(future)
+
+        if len(futures) == 0:
+            resultFace = pandas.ExcelWriter(faceRoot + os.sep + "tags.xlsx")
+            resultBody = pandas.ExcelWriter(bodyRoot + os.sep + "tags.xlsx")
+            pandas.DataFrame(resultFaceData, columns=columns1).to_excel(resultFace, index=False)
+            resultFace.save()
+            pandas.DataFrame(resultBodyData, columns=columns1).to_excel(resultBody, index=False)
+            resultBody.save()
 
     print('time2 end:', time.time() - tt)
